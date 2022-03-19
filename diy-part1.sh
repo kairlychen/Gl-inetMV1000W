e7df3f28f5aa615e083ddad57c9885905f32b3ad
ΦωΦ <8803509@qq.com>
2022-03-19 12:11:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/jerrykuku/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall

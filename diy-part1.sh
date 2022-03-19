9d555da8583dfe47e8922bc990f965df2fc586d9
ΦωΦ <8803509@qq.com>
2022-03-19 21:27:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,14 +12,10 @@
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-
-git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/jerrykuku/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
